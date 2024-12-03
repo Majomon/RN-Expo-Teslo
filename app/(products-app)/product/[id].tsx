b5d9036e01995e0d53657fee761a192b16e72faf
Mauri <mauri.monzon91@gmail.com>
2024-12-02 21:15:12 -0300

    Pänntalla de producto

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,11 +1,53 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { ThemedView } from "@/presentation/theme/components/ThemedView";
+import ThemedTextInput from "@/presentation/theme/components/ThemedTextInput";
 
 const ProductScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Ionicons name="camera-outline" size={25} />,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>ProductScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <ScrollView>
+        <ThemedView style={{ marginHorizontal: 10 }}>
+          <ThemedTextInput style={{ marginVertical: 5 }} placeholder="Títuto" />
+          <ThemedTextInput style={{ marginVertical: 5 }} placeholder="Slug" />
+          <ThemedTextInput
+            style={{ marginVertical: 5 }}
+            placeholder="Descripción"
+            multiline
+            numberOfLines={5}
+          />
+        </ThemedView>
+        <ThemedView
+          style={{
+            marginHorizontal: 10,
+            marginVertical: 5,
+            flexDirection: "row",
+            gap: 10,
+          }}
+        >
+          <ThemedTextInput style={{ flex: 1 }} placeholder="Precio" />
+          <ThemedTextInput style={{ flex: 1 }} placeholder="Inventario" />
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
