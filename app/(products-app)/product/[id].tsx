8e7b40badd9c3fda0740b1603f7c9101aed4ad5e
Mauri <mauri.monzon91@gmail.com>
2024-12-03 21:21:58 -0300

    Pantalla de camara

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,12 +1,12 @@
 import { Size } from "@/core/products/interfaces/product.interface";
 import { ProductImages } from "@/presentation/products/components/ProductImages";
 import { useProduct } from "@/presentation/products/hooks/useProduct";
+import { MenuIconButton } from "@/presentation/theme/components/MenuIconButton";
 import ThemedButton from "@/presentation/theme/components/ThemedButton";
 import { ThemedButtonGroup } from "@/presentation/theme/components/ThemedButtonGroup";
 import ThemedTextInput from "@/presentation/theme/components/ThemedTextInput";
 import { ThemedView } from "@/presentation/theme/components/ThemedView";
-import { Ionicons } from "@expo/vector-icons";
-import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
+import { Redirect, router, useLocalSearchParams, useNavigation } from "expo-router";
 import { Formik } from "formik";
 import React, { useEffect } from "react";
 import {
@@ -24,7 +24,7 @@ const ProductScreen = () => {
 
   useEffect(() => {
     navigation.setOptions({
-      headerRight: () => <Ionicons name="camera-outline" size={25} />,
+      headerRight: () => <MenuIconButton onPress={()=>router.push("/camera")}  icon="camera-outline"/>,
     });
   }, []);
 
